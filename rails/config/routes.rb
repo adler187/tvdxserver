aa8872dbd865e222458bc67c5bf4a996d93d2e94
Kevin Adler <adler_187@yahoo.com>
2010-12-04 05:16:31 +0000

    Added a home controller for showing the main page. This is what the 'user' views when he comes to the site. I also added an 'admin' controller that will link to all the admin functions.

--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -3,6 +3,8 @@ ActionController::Routing::Routes.draw do |map|
 
   map.resources :stations
 
+  map.resources :admin
+
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
@@ -35,7 +37,7 @@ ActionController::Routing::Routes.draw do |map|
   #   end
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-  map.root :controller => "stations"
+  map.root :controller => 'home'
 
   # See how all your routes lay out with "rake routes"
 
