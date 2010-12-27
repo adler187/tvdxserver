c4edcbd9bee08920504e493477774c43092a3781
Kevin Adler <adler_187@yahoo.com>
2010-12-27 21:27:42 +0000

    (re)Enabled zoom and location caching between requests

--- rails/app/controllers/home_controller.rb
+++ rails/app/controllers/home_controller.rb
@@ -1,22 +1,20 @@
 class HomeController < ApplicationController
-	def index
-		p params
-		
+	def index		
 		tuner_params = params[:tuner]
 		@tuner = tuner_params.nil? ? Tuner.first : Tuner.find(tuner_params[:id])
 		@logs = Log.all(:joins => ', stations', :select => 'logs.*, stations.*, max(logs.created_at) as log_time', :conditions => ['stations.id = logs.station_id and tuner_id = ?', @tuner.id], :group => 'callsign')
-# 		@logs = Log.all
-# 		if params[:location].nil?
-			@map_location = { 'latitude' => CONFIG['latitude'], 'longitude' => CONFIG['longitude'] }
-# 		else
-# 			@map_location = params[:location]
-# 		end
+
+		if params[:location].nil?
+			@map_location = { :latitude => CONFIG['latitude'], :longitude => CONFIG['longitude'] }
+		else
+			@map_location = params[:location]
+		end
 		
 
-# 		if params[:zoom].nil?
-# 			@zoom = params[:zoom]
-# 		else
+		if params[:zoom].nil?
+			@zoom = params[:zoom]
+		else
 			@zoom = 8
-# 		end
+		end
 	end
 end
