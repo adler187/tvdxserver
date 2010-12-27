6fa2bf80f845bf048855760a4b0c824530803682
Kevin Adler <adler_187@yahoo.com>
2010-12-27 22:16:18 +0000

    Fixed location issue on first load not correct

--- rails/app/controllers/home_controller.rb
+++ rails/app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
-	def index		
+	def index
 		tuner_params = params[:tuner]
 		@tuner = tuner_params.nil? ? Tuner.first : Tuner.find(tuner_params[:id])
 		@logs = Log.all(:joins => ', stations', :select => 'logs.*, stations.*, max(logs.created_at) as log_time', :conditions => ['stations.id = logs.station_id and tuner_id = ?', @tuner.id], :group => 'callsign')
@@ -9,14 +9,11 @@ class HomeController < ApplicationController
 		else
 			@map_location = params[:location]
 		end
-		
 
 		if params[:zoom].nil?
 			@zoom = 8
 		else
 			@zoom = params[:zoom]
 		end
-
-		p "Zoom is ", @zoom
 	end
 end
