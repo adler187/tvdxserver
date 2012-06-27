75e1f44d207f90b7c54e54d77a8ed957fab7f844
Kevin Adler <adler187@gmail.com>
2012-06-27 01:34:19 -0500

    Convert to jQuery
    - Convert home.js and index.js.erb to use jQuery
    - Clean up js to not muck with global namespace
    - Include jquery in home layout, won't need this come Rails 3.1

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -1,63 +1,60 @@
-
-document.observe
-(
-	"dom:loaded",
-	function()
+(function(){
+	
+	function handleResize()
 	{
+		var height = self.innerHeight - $('#top').offset().top - $('#content').offset().top;
+		$('#map').height(height + 'px');
+		$('#logs').height(height + 'px');
+	}
+
+	$(document).ready(function() {
 		handleResize();
 		
-		// get from rails somehow
-		var centerlatlng = new google.maps.LatLng($('latitude').content, $('longitude').content);
 		var myOptions =
 		{
 			zoom: 8,
-			center: centerlatlng,
+			center: new google.maps.LatLng($('#latitude').attr('content'), $('#longitude').attr('content')),
 			disableDefaultUI: true,
 			mapTypeId: google.maps.MapTypeId.ROADMAP
 		};
 		
-		map = new google.maps.Map($("map"), myOptions);
+		map = new google.maps.Map($('#map')[0], myOptions);
 		
 		google.maps.event.addListener(map, 'click', map.clearActiveMarker.bind(map));
 		
-		Event.observe
-		(
-			'options_form',
-			'submit',
-			function(event)
+		$('#options_form').submit(function(event) {
+			form = this;
+			var throbber = $('#throbber');
+			
+			throbber.removeClass('hidden');
+			$(form).attr('disabled', 'disabled');
+										
+			$.ajax({
+				dataType: 'script',
+				type: 'POST',
+				url: form.action,
+				data: $(form).serialize(),
+				complete: function() {
+					throbber.addClass('hidden');
+					$(form).removeAttr('disabled');
+				}
+			});
+			
+			return false;
+		});
+		
+		
+		$('#options_form').children().each(function(){
+			if(this.tagName == 'SELECT')
 			{
-				var form = $('options_form');
-				var throbber = $('throbber');
-
-				form.request
-				(
-					{
-						onCreate: function()
-						{
-							throbber.removeClassName('hidden');
-							form.disable();
-						},
-						onComplete: function()
-						{
-							throbber.addClassName('hidden');
-							form.enable();
-						}
-					}
-				);
-				event.stop();
+				$(this).change(function() { $('#submit').click(); });
 			}
-		);
+		});
+		
+		$('#submit').click();
+	});
 
-		$('submit').click();
-	}
-);
-
-function handleResize()
-{
-	var height = self.innerHeight - $('top').offsetHeight - 30;
-	$('map').style.height = height + 'px';
-	$('logs').style.height = height + 'px';
-}
-
-Event.observe(window, 'resize', handleResize);
+	$(window).bind('resize', handleResize);
+	
+})();
 
