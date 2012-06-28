ef61e1727e2967588f4c76a1344b546e3556dfe5
Kevin Adler <adler187@gmail.com>
2012-06-28 00:19:06 -0500

    Converted RAILS_ENV to Rails.env, since RAILS_ENV was removed

--- config/initializers/load_config.rb
+++ config/initializers/load_config.rb
@@ -1,3 +1,3 @@
 require 'yaml'
 
-CONFIG = YAML.load_file(File.join(Rails.root, 'config', 'config.yml'))[RAILS_ENV]
+CONFIG = YAML.load_file(File.join(Rails.root, 'config', 'config.yml'))[Rails.env]
