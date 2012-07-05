fb46b0fac07e7af6ceffbfa810fb3f6ce5761ff4
Kevin Adler <adler187@gmail.com>
2012-07-05 00:39:32 -0500

    Update to Rails 3.2

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'http://rubygems.org'
 
-gem 'rails', '~> 3.1.3'
+gem 'rails', '~> 3.2.0'
 
 group :development, :test do
 	gem 'sqlite3'
@@ -29,9 +29,9 @@ end
 # end
 
 #group :assets do
-#  gem 'sass-rails', '~> 3.1.0'
-#  gem 'coffee-rails', '~> 3.1.0'
-#  gem 'uglifier'
+#  gem 'sass-rails', '~> 3.2.3'
+#  gem 'coffee-rails', '~> 3.2.1'
+#  gem 'uglifier', '>= 1.0.3'
 #end
 
 gem 'jquery-rails'
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,5 +27,12 @@ Tvscanner::Application.configure do
   
   # Expand the lines which load assets
 #   config.assets.debug = true
+  
+  # Raise exception on mass assignment for Active Record models
+  config.active_record.mass_assignment_sanitizer = :strict
+  
+  # Log the query plan for queries taking more than this (works
+  # with SQLite, MySQL, and PostgreSQL)
+  config.active_record.auto_explain_threshold_in_seconds = 0.5
 end
 
