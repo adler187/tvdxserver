dac0efcefa9a5821e0c88b9c2c61256f0ad48fb2
Kevin Adler <adler187@gmail.com>
2012-07-05 03:35:36 -0500

    Fix typo Noize -> Noise

--- lib/maps.rb
+++ lib/maps.rb
@@ -19,7 +19,7 @@ def info_window(log)
 	content += 'Distance: ' + station.distance.round(2).to_s + ' mi<br />'
 	content += 'Most recent scan: <br />'
 	content += 'Signal Strength: ' + log.signal_strength.to_s + '<br />'
-	content += 'Signal to Noize: ' + log.signal_to_noise.to_s + '<br />'
+	content += 'Signal to Noise: ' + log.signal_to_noise.to_s + '<br />'
 	content += 'Signal Quality: ' + log.signal_quality.to_s + '<br />'
 	content += link_to 'Signal Graph', "/chart/#{station.id}/#{log.tuner.id}", { :target => 'signal_graph' }
 end
