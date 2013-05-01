e0ccdf90625f66ebb2da8779983cebe026bef480
Kevin Adler <adler_187@yahoo.com>
2013-04-30 22:26:10 -0500

    Don't require CSRF validation for JSON requests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
+  
+  skip_before_filter :verify_authenticity_token, :if => lambda { request.content_type == "application/json" }
 
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
