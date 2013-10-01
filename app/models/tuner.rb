409e7cd1946d85ad7699e572cd3c4efe6511ce39
Kevin Adler <adler_187@yahoo.com>
2013-09-30 22:28:09 -0500

    Clean up tuner info records on delete

--- app/models/tuner.rb
+++ app/models/tuner.rb
@@ -7,6 +7,7 @@ class Tuner < ActiveRecord::Base
 
   acts_as_list
   default_scope :order => "position"
+  before_destroy :clear_tuner_infos
   
   def current
     tuner_info[tuner_info.length - 1]
@@ -22,5 +23,10 @@ class Tuner < ActiveRecord::Base
     query = query.where('created_at > ? ', time_interval.date_range.begin.utc) unless time_interval.all_interval?
     
     return query
+  end
+
+protected
+  def clear_tuner_infos
+    tuner_info.clear
   end
 end
