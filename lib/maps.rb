345c6093c04a84877aff7589b5687c5a70828b66
Kevin Adler <adler_187@yahoo.com>
2014-04-23 02:40:13 -0500

    Cleanup info window

--- lib/maps.rb
+++ lib/maps.rb
@@ -12,14 +12,19 @@ end
 def info_window(log)
   station = log.station
   
-  content = ''
-  content += link_to station.callsign, "http://www.rabbitears.info/market.php?request=station_search&callsign=#{station.callsign}#station", { :target => 'station_lookup' }
-  content += '<br /><hr>Last received on ' + log.created_at.to_s + '<br />'
-  content += 'Channel: ' + station.rf.to_s + '<br />'  
-  content += 'Distance: ' + station.distance.round(2).to_s + ' mi<br />'
-  content += 'Most recent scan: <br />'
-  content += 'Signal Strength: ' + log.signal_strength.to_s + '<br />'
-  content += 'Signal to Noise: ' + log.signal_to_noise.to_s + '<br />'
-  content += 'Signal Quality: ' + log.signal_quality.to_s + '<br />'
+  content = link_to station.callsign, "http://www.rabbitears.info/market.php?request=station_search&callsign=#{station.callsign}#station", { :target => 'station_lookup' }
+  content += raw <<eos
+<br />
+<hr>
+Last received on #{log.created_at.to_s}<br />
+Channel: #{station.rf.to_s}<br />
+Distance: #{station.distance.round(2).to_s} mi<br />
+Most recent scan: <br />
+Signal Strength: #{log.signal_strength.to_s}<br />
+Signal to Noise: #{log.signal_to_noise.to_s}<br />
+Signal Quality: #{log.signal_quality.to_s}<br />
+eos
+
   content += link_to 'Signal Graph', "/chart/#{station.id}/#{log.tuner.id}", { :target => 'signal_graph' }
+  content.gsub("\n", '\n')
 end
